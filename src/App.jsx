10f9c00239d7beb6af6f7f399b78126abbbe8217
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2024-03-13 19:55:49 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>DEPLOY EN VERCEL</h1>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
